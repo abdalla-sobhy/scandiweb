12dc0d53e6f1f209ad2b69ae124f3e4cf7c2a458
omar tamer <omartamerdev@gmail.com>
2025-02-26 23:32:53 +0200

    aaa

--- scandiweb/vite.config.ts
+++ scandiweb/vite.config.ts
@@ -13,4 +13,11 @@ export default defineConfig({
     target: "es2022",
     cssCodeSplit: false,
   },
+  server: {
+    cors: {
+      origin: "*",
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    },
+  },
 });
