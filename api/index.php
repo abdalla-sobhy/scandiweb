a8cdd3d1fe93c151a65a90f30ee87b65ac0efde8
omar tamer <omartamerdev@gmail.com>
2025-02-27 00:21:16 +0200

    a

--- api/index.php
+++ api/index.php
@@ -2,6 +2,10 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/GraphQL/Schema.php';
 require_once __DIR__ . '/config/DbConnect.php'; 
