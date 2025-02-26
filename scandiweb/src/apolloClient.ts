e39546792e6f89f67436a54328b6682a40ee2e5a
omar tamer <omartamerdev@gmail.com>
2025-02-26 22:28:17 +0200

    aa

--- scandiweb/src/apolloClient.ts
+++ scandiweb/src/apolloClient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
-  uri: 'http://18.205.22.15:8000/api/graphql.php',
+  uri: 'http://18.205.22.15:8000/api/graphql.php', // Changes it when changing server
   cache: new InMemoryCache(),
 });
 
