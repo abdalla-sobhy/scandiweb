a4d56b1b1e9f7b490e37d6b7136160207bbd349d
abdalla-sobhy <abdallahbs9393@gmail.com>
2025-02-27 13:01:14 +0200

    url update

--- scandiweb/src/apolloClient.ts
+++ scandiweb/src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:80/scandiweb_test/api/graphql.php',
+  uri: 'http://localhost:80/scandiweb_test/api/index.php',
   cache: new InMemoryCache(),
 });
 
