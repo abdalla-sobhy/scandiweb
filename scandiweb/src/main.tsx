c182e8b981f0117547027b6b4e3b13389088f691
abdalla-sobhy <abdallahbs9393@gmail.com>
2025-03-19 22:40:03 +0200

    a

--- scandiweb/src/main.tsx
+++ scandiweb/src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ProductProvider } from "./components//ProductContext.tsx";
 import { ApolloProvider } from '@apollo/client';
 import client from './apolloClient';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ApolloProvider client={client}>
+      <ProductProvider>
           <App />
+      </ProductProvider>
     </ApolloProvider>
   </StrictMode>,
 )
