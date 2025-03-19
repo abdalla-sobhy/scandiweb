a5ae232a878e1b78e60f21c69ac3d46e5256ba71
abdalla-sobhy <abdallahbs9393@gmail.com>
2025-03-19 22:32:38 +0200

    a

--- scandiweb/src/App.tsx
+++ scandiweb/src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Index from './pages/Index.tsx'
 import ProductPage from './pages/ProductPage.tsx';
-import { ProductProvider } from "./components/ProductContext";
 
 
 function App() {
@@ -10,13 +9,11 @@ function App() {
   return (
     <>
       <Router>
-          <ProductProvider>
             <Routes>
               <Route path="/" element={<Navigate to="/Index" />} />
               <Route path="/Index" element={<Index />} />
               <Route path="/ProductPage" element={<ProductPage />} />
             </Routes>
-          </ProductProvider>
       </Router>
     </>
   )
