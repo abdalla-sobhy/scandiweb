5da3facbdfda9d26308f4d50f9fdaab0ddc9abe7
abdalla-sobhy <abdallahbs9393@gmail.com>
2025-03-20 15:59:13 +0200

    a

--- api/Models/AttributeModel.php
+++ api/Models/AttributeModel.php
@@ -1,14 +1,11 @@
 <?php
-require_once 'AbstractModel.php';
-
+namespace App\Models;
 abstract class AttributeModel extends AbstractModel {
     protected $type;
-    
-    public function __construct(PDO $pdo, string $type) {
+    public function __construct(\PDO $pdo, string $type) {
         parent::__construct($pdo);
         $this->type = $type;
     }
-    
     abstract public function getAll();
     abstract public function getById($id);
 }
